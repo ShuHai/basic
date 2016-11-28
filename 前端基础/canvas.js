956837b5fb0fb4914f9e0c6608c133d0b349cf5b
xiaowei.yang <xiaowei.yang@ele.me>
2016-11-28 18:12:24 +0800

    init canvas

---
+++ 前端基础/canvas.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+// 填充颜色
+ctx.fillStyle = "green";
+// 轮廓颜色,线条颜色
+ctx.strokeStyle = "green";
+// 填充路径
+ctx.fillRect(10, 10, 100, 100);
+// 设置线条宽度
+lineWidth = 2;
+// 一个标准流程
+context.beginPath();
+context.strokeStyle = '#66ccff';
+context.moveTo(x,y);
+context.lineTo(x2,y2);
+context.stroke();
