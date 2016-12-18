f5d81a5d9ea65898a844828497eafd6a1cf3a422
xiaowei.yang <xiaowei.yang@ele.me>
2016-12-18 18:50:04 +0800

    update

--- 前端基础/object.js
+++ 前端基础/object.js
@@ -15,3 +15,19 @@ Object.defineProperty(userInfo, "nickName", {
         document.getElementById('nickName').innerHTML = nick;
     }
 });
+
+//深克隆
+var cloneObj = function(obj){
+   var str, newobj = obj.constructor === Array ? [] : {};
+   if(typeof obj !== 'object'){
+     return;
+   } else if(window.JSON){
+     str = JSON.stringify(obj), //系列化对象
+    newobj = JSON.parse(str); //还原
+ } else {
+    for(var i in obj){
+     newobj[i] = typeof obj[i] === 'object' ? cloneObj(obj[i]) : obj[i];
+      }
+    }
+    return newobj;
+};
